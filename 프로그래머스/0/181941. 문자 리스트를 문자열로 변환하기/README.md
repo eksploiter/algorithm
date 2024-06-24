0e68f3db42f4f5638a3f85a3982e882a71ddfa2b
MinSoo <136697128+thanx-To-Dev-Minsoo@users.noreply.github.com>
2024-06-24 20:57:13 +0900

    [level 0] Title: 문자 리스트를 문자열로 변환하기, Time: 0.20 ms, Memory: 82.8 MB -BaekjoonHub

--- 프로그래머스/0/181941. 문자 리스트를 문자열로 변환하기/README.md
+++ 프로그래머스/0/181941. 문자 리스트를 문자열로 변환하기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 문자 리스트를 문자열로 변환하기 - 181941 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181941?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181941) 
 
 ### 성능 요약
 
-메모리: 72.8 MB, 시간: 0.04 ms
+메모리: 82.8 MB, 시간: 0.20 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 24일 20:54:07
+2024년 06월 24일 20:57:12
 
 ### 문제 설명
 
--- 프로그래머스/0/181941. 문자 리스트를 문자열로 변환하기/문자 리스트를 문자열로 변환하기.java
+++ 프로그래머스/0/181941. 문자 리스트를 문자열로 변환하기/문자 리스트를 문자열로 변환하기.java
@@ -1,20 +1,6 @@
 class Solution {
     public String solution(String[] arr) {
-        // StringBuilder 객체를 생성하여 효율적으로 문자열을 이어 붙입니다.
-        StringBuilder sb = new StringBuilder();
-        
-        // 배열의 모든 원소를 순회하면서 StringBuilder에 추가합니다.
-        for (String s : arr) {
-            sb.append(s);
-        }
-        
-        // StringBuilder에 누적된 문자열을 최종적으로 반환합니다.
-        return sb.toString();
+        String answer = "";
+        return String.join("",arr);
     }
 }
-
-/*class Solution { // 다른 풀이
-    public String solution(String[] arr) {
-        return String.join("", arr);
-    }
-}*/
