9b584b758bb25fc6e1199573ce60af451170e438
MinSoo <136697128+thanx-To-Dev-Minsoo@users.noreply.github.com>
2024-07-05 16:23:24 +0900

    [Bronze V] Title: A/B, Time: 212 ms, Memory: 17740 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Scanner 객체 생성
+        Scanner scanner = new Scanner(System.in);
+        
+        // 두 정수 A와 B 입력받기
+        double A = scanner.nextInt();
+        double B = scanner.nextInt();
+        
+        // A와 B를 더하여 결과 출력
+        System.out.println(A / B);
+        
+        // Scanner 닫기
+        scanner.close();
+    }
+}
