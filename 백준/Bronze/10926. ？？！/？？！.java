e5cf1f2aaa42636838bb277afa0bbab0bd305e68
MinSoo <136697128+thanx-To-Dev-Minsoo@users.noreply.github.com>
2024-07-08 17:27:47 +0900

    [Bronze V] Title: ??!, Time: 204 ms, Memory: 17708 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        String a = sc.next();
+        
+        System.out.println(a + "??!");
+        
+        sc.close();
+    }
+}
