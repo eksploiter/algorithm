7bcd6c2d2206d1e6599491c5bda76c5754ad839a
MinSoo <136697128+thanx-To-Dev-Minsoo@users.noreply.github.com>
2024-07-13 01:14:58 +0900

    [Bronze V] Title: 합, Time: 172 ms, Memory: 17564 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int a = sc.nextInt();
+        int sum = 0;
+        
+        for (int i = 1; i <= a; i++) {
+            sum += i;
+        }
+        
+        System.out.println(sum);
+        
+        sc.close();
+    }
+}
