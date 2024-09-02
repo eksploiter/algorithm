e751241647f0c5f847dbe7a62ac06e3167aa22df
MinSoo <minsu_510@naver.com>
2024-09-03 00:49:55 +0900

    [Bronze III] Title: A+B - 6, Time: 180 ms, Memory: 17860 KB -BaekjoonHub

--- 백준/Bronze/10953. A＋B － 6/A＋B － 6.java
+++ 백준/Bronze/10953. A＋B － 6/A＋B － 6.java
@@ -8,21 +8,18 @@ public class Main {
         int T = sc.nextInt();
         sc.nextLine();  // 개행 문자 처리
         
-        // 각 테스트 케이스의 결과를 저장할 배열
-        int[] results = new int[T];
-        
-        // 각 테스트 케이스마다 A와 B를 입력받아 결과를 계산 후 저장
+        // 각 테스트 케이스마다 A와 B를 입력받아 처리
         for (int i = 0; i < T; i++) {
+            // 각 줄에 A,B 형태로 입력이 들어옴
             String input = sc.nextLine();
+            
+            // 콤마(,)로 분리하여 A와 B를 추출
             String[] numbers = input.split(",");
             int A = Integer.parseInt(numbers[0]);
             int B = Integer.parseInt(numbers[1]);
-            results[i] = A + B;
-        }
-        
-        // 모든 입력이 끝난 후에 결과를 출력
-        for (int result : results) {
-            System.out.println(result);
+            
+            // A + B를 출력
+            System.out.println(A + B);
         }
         
         sc.close();
