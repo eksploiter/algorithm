0cd52be05a50a5645be7ff2ff53d83033df54684
MinSoo <minsu_510@naver.com>
2024-09-18 00:06:27 +0900

    [Bronze V] Title: 아스키 코드, Time: 160 ms, Memory: 17592 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    
+        char word = scanner.next().charAt(0);
+    
+        System.out.println((int)word);
+    
+        scanner.close();
+    }
+}
