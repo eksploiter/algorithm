ee8a7e993dc1a81e24659e8a3883969a3a1d5fea
MinSoo <minsu_510@naver.com>
2024-09-19 00:38:35 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 184 ms, Memory: 18424 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -3,26 +3,25 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         String S = scanner.nextLine();
-
-        int[] positions = new int[26];
+        
+        int[] word = new int[26];
+        
         for (int i = 0; i < 26; i++) {
-            positions[i] = -1;
+            word[i] = -1;
         }
-
-        for (int i = 0; i< S.length(); i++) {
+        
+        for (int i = 0; i < S.length(); i++) {
             char ch = S.charAt(i);
             int index = ch - 'a';
-
-            // 처음 등장한 경우에만 위치 저장
-            if (positions[index] == -1) {
-                positions[index] = i;
+            
+            if (word[index] == -1) {
+                word[index] = i;
             }
         }
-
+        
         for (int i = 0; i < 26; i++) {
-            System.out.print(positions[i] + " ");
+            System.out.print(word[i] + " ");
         }
     }
 }
