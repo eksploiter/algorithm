8f645190fd139ac54e666b0f507e129a5b5850e2
MinSoo <minsu_510@naver.com>
2024-09-20 23:13:39 +0900

    [Bronze II] Title: 단어의 개수, Time: 488 ms, Memory: 62616 KB -BaekjoonHub

--- 백준/Bronze/1152. 단어의 개수/단어의 개수.java
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -8,7 +8,7 @@ public class Main {
         if (words.isEmpty()) {
             System.out.println(0);  // 입력이 공백만 있는 경우
         } else {
-            String[] wordArray = words.split("\\s+");  // 공백을 기준으로 단어 나누기
+            String[] wordArray = words.split(" ");  // 공백을 기준으로 단어 나누기
             System.out.println(wordArray.length);  // 단어의 개수 출력
         }
     }
