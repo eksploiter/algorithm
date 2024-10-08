fde0364dec2d7a5d9188dfaf42f3e0c7d9dbeb43
MinSoo <minsu_510@naver.com>
2024-10-09 01:04:19 +0900

    [Silver V] Title: 색종이, Time: 176 ms, Memory: 17732 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.java
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -2,20 +2,15 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         
-        // 100x100 크기의 도화지
         boolean[][] paper = new boolean[100][100];
+        int T = scanner.nextInt();
         
-        // 색종이의 수 입력
-        int n = sc.nextInt();
-        
-        // 색종이를 도화지에 붙이기
-        for (int i = 0; i < n; i++) {
-            int x = sc.nextInt();  // 색종이의 왼쪽 변과 도화지의 왼쪽 변 사이의 거리
-            int y = sc.nextInt();  // 색종이의 아래쪽 변과 도화지의 아래쪽 변 사이의 거리
+        for (int i = 0; i < T; i++) {
+            int x = scanner.nextInt();
+            int y = scanner.nextInt();
             
-            // 도화지의 해당 영역을 10x10 크기로 1로 표시
             for (int j = x; j < x + 10; j++) {
                 for (int k = y; k < y + 10; k++) {
                     paper[j][k] = true;
@@ -23,19 +18,15 @@ public class Main {
             }
         }
         
-        // 검은 영역의 넓이 계산
         int area = 0;
         for (int i = 0; i < 100; i++) {
             for (int j = 0; j < 100; j++) {
-                if (paper[i][j]) {
+                if (paper[i][j] == true) {
                     area++;
                 }
             }
         }
         
-        // 결과 출력
         System.out.println(area);
-        
-        sc.close();
     }
 }
