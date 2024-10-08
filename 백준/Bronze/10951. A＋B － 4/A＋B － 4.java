e8102b72a8d989dbf62b70eb1f932a8f733e0d2f
MinSoo <minsu_510@naver.com>
2024-10-09 01:42:53 +0900

    [Bronze V] Title: A+B - 4, Time: 180 ms, Memory: 17832 KB -BaekjoonHub

--- 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
@@ -2,16 +2,13 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         
-        // 입력이 있는 동안 계속 반복
-        while (sc.hasNext()) {
-            int A = sc.nextInt();  // 첫 번째 정수 입력
-            int B = sc.nextInt();  // 두 번째 정수 입력
+        while (scanner.hasNext()) {
+            int A = scanner.nextInt();
+            int B = scanner.nextInt();
             
-            System.out.println(A + B);  // A + B 결과 출력
+            System.out.println(A + B);
         }
-        
-        sc.close();  // Scanner 닫기
     }
 }
