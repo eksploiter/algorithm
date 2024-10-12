467d90e4f3f986ca5de13aa5c42c17b652833419
eksploiter <minsu_510@naver.com>
2024-10-13 00:33:42 +0900

    [Bronze I] Title: 단어 공부, Time: 440 ms, Memory: 31888 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.java
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -1,29 +1,35 @@
-import java.util.*;
+import java.util.Scanner;
 
-public class Main{
+public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
         String word = scanner.nextLine().toUpperCase();
         
-        int[] freq = new int[26];
+        int[] freq = new int[26]; // AAB
         
         for (int i = 0; i < word.length(); i++) {
             char ch = word.charAt(i);
             freq[ch - 'A']++;
         }
         
-        int maxFreq = -1;
-        char mostFreq = '?';
+        // 1. A -> 0
+        // 2. A -> 0 // ++ 이므로 1
+        // 3. B -> 1 // ++ 이므로 0
+        
+        // int[26] freq = {1, 0, -1, -1, ...}
+        
+        int maxFreq = -1; // 어떠한 알파벳도 고려되지 않음
+        char mostFreqChar = '?'; // 초가 값
         for (int i = 0; i < freq.length; i++) {
             if (freq[i] > maxFreq) {
-                maxFreq = freq[i];
-                mostFreq = (char) (i + 'A');
-            } else if (freq[i] == maxFreq) {
-                mostFreq = '?';
+                maxFreq = freq[i]; // maxFreq = 1
+                mostFreqChar = (char) (i + 'A'); // 정수형에서 문자형으로 형변환 -> A
+            } else if (freq[i] == maxFreq) { // 더이상 겹치는 값이 없다.
+                mostFreqChar = '?';
             }
         }
         
-        System.out.println(mostFreq);
+        System.out.println(mostFreqChar);
     }
 }
