22ba5fc23cf13a1228e1e35b1cfb385a8b0eab89
eksploiter <minsu_510@naver.com>
2024-10-15 23:24:28 +0900

    [Bronze IV] Title: 알고리즘 수업 - 알고리즘의 수행 시간 2, Time: 168 ms, Memory: 17668 KB -BaekjoonHub

---
+++ 백준/Bronze/24263. 알고리즘 수업 － 알고리즘의 수행 시간 2/알고리즘 수업 － 알고리즘의 수행 시간 2.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int n = scanner.nextInt();
+        
+        System.out.println(n);
+        System.out.println(1);
+        
+        scanner.close();
+    }
+}
