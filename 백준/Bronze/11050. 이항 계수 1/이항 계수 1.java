722a0b0bcccad8e93e64dd69609559005974c5e5
eksploiter <minsu_510@naver.com>
2024-11-01 00:38:41 +0900

    [Bronze I] Title: 이항 계수 1, Time: 180 ms, Memory: 17676 KB -BaekjoonHub

--- 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
@@ -3,22 +3,22 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        
         int N = scanner.nextInt();
         int K = scanner.nextInt();
-
+        
         int up = 1;
         int down = 1;
-
+        
         for (int i = 0; i < K; i++) {
             up *= (N - i);
             down *= (K - i);
         }
-
+        
         int answer = up / down;
-
+        
         System.out.println(answer);
-
+        
         scanner.close();
     }
 }
