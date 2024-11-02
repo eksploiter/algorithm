17e205652f28578f1138a70e7cf7767cf15bcf4b
eksploiter <minsu_510@naver.com>
2024-11-03 00:01:27 +0900

    [D2] Title: [S/W 문제해결 기본] 1일차 - 최빈수 구하기, Time: 184 ms, Memory: 41,612 KB -BaekjoonHub

--- SWEA/D2/1204. ［S／W 문제해결 기본］ 1일차 － 최빈수 구하기/［S／W 문제해결 기본］ 1일차 － 최빈수 구하기.java
+++ SWEA/D2/1204. ［S／W 문제해결 기본］ 1일차 － 최빈수 구하기/［S／W 문제해결 기본］ 1일차 － 최빈수 구하기.java
@@ -1,43 +1,38 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Solution {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int T = sc.nextInt();
+	public static void main(String[] args) {
+    	Scanner scanner = new Scanner(System.in);
         
-        for (int t = 1; t <= T; t++) {
-            int caseNum = sc.nextInt();
+        int T = scanner.nextInt();
+        for (int i = 0; i < T; i++) {
+        	int testCase = scanner.nextInt();
             int[] scores = new int[1000];
-            
-            for (int i = 0; i < 1000; i++) {
-                scores[i] = sc.nextInt();
+            for (int j = 0; j < 1000; j++) {
+            	scores[j] = scanner.nextInt();
             }
-            
-            System.out.println("#" + caseNum + " " + findMode(scores));
+            System.out.println("#" + testCase + " " + findMode(scores));
         }
-        
-        sc.close();
+        scanner.close();
     }
+    
     public static int findMode(int[] scores) {
-        // 점수는 0~100이므로 크기 101의 배열 사용
-        int[] count = new int[101];
+    	int[] count = new int[101];
         
-        // 점수 빈도 계산
         for (int score : scores) {
-            count[score]++;
+        	count[score]++;
         }
         
         int maxCount = 0;
         int mode = 0;
-        
-        // 큰 점수부터 확인하여 최빈수 찾기 (내림차순)
-        for (int i = 100; i >= 0; i--) {
-            if (count[i] > maxCount) {
-                maxCount = count[i];
-                mode = i;
+        for (int i = 0; i < count.length; i++) {
+        	if (count[i] > maxCount) {
+            	maxCount = count[i]; // 가장 큰 빈도수 
+                mode = i; // 가장 큰 빈도수를 가진 점수
+            } else if (count[i] == maxCount && i > mode) {
+            	mode = i;
             }
         }
-        
         return mode;
     }
 }
