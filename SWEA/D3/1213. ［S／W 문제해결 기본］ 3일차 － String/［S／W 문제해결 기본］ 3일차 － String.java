eda992c8dd36a39b10eb385d7d13f40c68fa96f7
eksploiter <minsu_510@naver.com>
2024-11-02 20:24:13 +0900

    [D3] Title: [S/W 문제해결 기본] 3일차 - String, Time: 139 ms, Memory: 21,680 KB -BaekjoonHub

--- SWEA/D3/1213. ［S／W 문제해결 기본］ 3일차 － String/［S／W 문제해결 기본］ 3일차 － String.java
+++ SWEA/D3/1213. ［S／W 문제해결 기본］ 3일차 － String/［S／W 문제해결 기본］ 3일차 － String.java
@@ -1,37 +1,32 @@
 import java.util.*;
 
 public class Solution {
-    
     public static void main(String[] args) {
     	Scanner scanner = new Scanner(System.in);
         
         int T = 10;
         
-    	for (int i = 1; i <= T; i++) {
-            scanner.nextLine(); // 개행문자 처리
-            String pattern = scanner.nextLine().trim();
-        	String text = scanner.nextLine().trim();
-        	System.out.println("#" + i + " " + bruteForce(pattern, text));
+        for (int i = 1; i <= 10; i++) {
+            scanner.nextLine();
+            String pattern = scanner.nextLine();
+            String text = scanner.nextLine();
+            System.out.println("#" + i + " " + bruteForce(text, pattern));
         }
-        
-        scanner.close();
     }
     
-    public static int bruteForce(String pattern, String text) {
+    public static int bruteForce(String text, String pattern) {
     	int count = 0;
-        
         for (int i = 0; i <= text.length() - pattern.length(); i++) {
         	boolean found = true;
-            
             for (int j = 0; j < pattern.length(); j++) {
             	if (text.charAt(i + j) != pattern.charAt(j)) {
                 	found = false;
                     break;
                 }
             }
-            if (found) {
-                count++;
-            }   
+            if (found) { 
+            	count++;
+            }
         }
         return count;
     }
