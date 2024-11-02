3751443db83ceb717f8e66645e3a7b58ea2616f5
eksploiter <minsu_510@naver.com>
2024-11-03 00:40:53 +0900

    [D3] Title: [S/W 문제해결 기본] 3일차 - String, Time: 207 ms, Memory: 20,736 KB -BaekjoonHub

--- SWEA/D3/1213. ［S／W 문제해결 기본］ 3일차 － String/［S／W 문제해결 기본］ 3일차 － String.java
+++ SWEA/D3/1213. ［S／W 문제해결 기본］ 3일차 － String/［S／W 문제해결 기본］ 3일차 － String.java
@@ -3,18 +3,18 @@ import java.util.*;
 public class Solution {
 	public static void main(String[] args) {
     	Scanner scanner = new Scanner(System.in);
-        
         int T = 10;
-        for (int i = 1; i <= T; i++) {
+        
+        for (int i = 1; i <= 10; i++) {
         	scanner.nextLine();
             String pattern = scanner.nextLine();
             String text = scanner.nextLine();
             System.out.println("#" + i + " " + bruteForce(text, pattern));
         }
-        scanner.close();
     }
+    
     public static int bruteForce(String text, String pattern) {
-    	int count = 0;
+    	int count = 0; 
         for (int i = 0; i <= text.length() - pattern.length(); i++) {
         	boolean found = true;
             for (int j = 0; j < pattern.length(); j++) {
