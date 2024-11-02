657e495d1bac8887b48fd12392d6fe4c1d6d8f3f
eksploiter <minsu_510@naver.com>
2024-11-03 01:55:19 +0900

    [D2] Title: [S/W 문제해결 기본] 1일차 - 최빈수 구하기, Time: 201 ms, Memory: 40,532 KB -BaekjoonHub

--- SWEA/D2/1204. ［S／W 문제해결 기본］ 1일차 － 최빈수 구하기/［S／W 문제해결 기본］ 1일차 － 최빈수 구하기.java
+++ SWEA/D2/1204. ［S／W 문제해결 기본］ 1일차 － 최빈수 구하기/［S／W 문제해결 기본］ 1일차 － 최빈수 구하기.java
@@ -3,32 +3,28 @@ import java.util.*;
 public class Solution {
 	public static void main(String[] args) {
     	Scanner scanner = new Scanner(System.in);
-        
         int T = scanner.nextInt();
-        for (int i = 0; i < T; i++) {
-        	int testCase = scanner.nextInt();
+        for (int i = 1; i <= T; i++) {
+        	scanner.nextLine();
             int[] scores = new int[1000];
             for (int j = 0; j < 1000; j++) {
             	scores[j] = scanner.nextInt();
             }
-            System.out.println("#" + testCase + " " + findMode(scores));
+            System.out.println("#" + i + " " + findMode(scores));
         }
         scanner.close();
     }
-    
     public static int findMode(int[] scores) {
-    	int[] count = new int[101];
-        
+    	int[] count = new int[101]; // 0-100
         for (int score : scores) {
         	count[score]++;
         }
-        
         int maxCount = 0;
         int mode = 0;
         for (int i = 0; i < count.length; i++) {
         	if (count[i] > maxCount) {
-            	maxCount = count[i]; // 가장 큰 빈도수 
-                mode = i; // 가장 큰 빈도수를 가진 점수
+            	maxCount = count[i];
+                mode = i;
             } else if (count[i] == maxCount && i > mode) {
             	mode = i;
             }
