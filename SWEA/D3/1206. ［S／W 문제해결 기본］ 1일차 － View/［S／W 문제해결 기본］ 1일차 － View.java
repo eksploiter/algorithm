481aa28fc3258d02d62fec8bc4351220f39fb609
eksploiter <minsu_510@naver.com>
2024-11-03 13:44:01 +0900

    [D3] Title: [S/W 문제해결 기본] 1일차 - View, Time: 198 ms, Memory: 33,856 KB -BaekjoonHub

--- SWEA/D3/1206. ［S／W 문제해결 기본］ 1일차 － View/［S／W 문제해결 기본］ 1일차 － View.java
+++ SWEA/D3/1206. ［S／W 문제해결 기본］ 1일차 － View/［S／W 문제해결 기본］ 1일차 － View.java
@@ -4,21 +4,20 @@ public class Solution {
 	public static void main(String[] args) {
     	Scanner scanner = new Scanner(System.in);
         int T = 10;
-        for (int t = 1; t  <= 10; t++) {
-            int N = scanner.nextInt();
-            int[] building = new int[N];
-            for (int i = 0; i < N; i++) {
-            	building[i] = scanner.nextInt();
+        for (int i = 1; i <= 10; i++) {
+        	int N = scanner.nextInt();
+            int[] buildings = new int[N];
+            for (int j = 0; j < N; j++) {
+            	buildings[j] = scanner.nextInt();
             }
-            int view = 0;
-            for (int i = 2; i < N - 2; i++) {
-            	int max = Math.max(Math.max(building[i - 2], building[i - 1]), Math.max(building[i + 1], building[i + 2]));
-                
-                if (building[i] > max) {
-                	view += (building[i] - max);
+            int bestView = 0;
+            for (int j = 2; j < N - 2; j++) {
+            	int view = Math.max(Math.max(buildings[j - 1], buildings[j - 2]), Math.max(buildings[j + 1], buildings[j + 2]));
+                if (buildings[j] > view) {
+                	bestView += (buildings[j] - view);
                 }
             }
-            System.out.println("#" + t + " " + view);
+            System.out.println("#" + i + " " + bestView);
         }
         scanner.close();
     }
