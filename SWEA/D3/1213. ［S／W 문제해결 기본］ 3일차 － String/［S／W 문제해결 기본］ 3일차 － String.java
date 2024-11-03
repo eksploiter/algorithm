61145c443f69910c8b0d017b0c4624d715431c45
eksploiter <minsu_510@naver.com>
2024-11-03 17:43:23 +0900

    [D3] Title: [S/W 문제해결 기본] 3일차 - String, Time: 147 ms, Memory: 22,272 KB -BaekjoonHub

--- SWEA/D3/1213. ［S／W 문제해결 기본］ 3일차 － String/［S／W 문제해결 기본］ 3일차 － String.java
+++ SWEA/D3/1213. ［S／W 문제해결 기본］ 3일차 － String/［S／W 문제해결 기본］ 3일차 － String.java
@@ -1,23 +1,23 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class Solution {
 	public static void main(String[] args) {
     	Scanner scanner = new Scanner(System.in);
         int T = 10;
-    	for (int i = 1; i <= 10; i++) {
+        for (int i = 1; i <= 10; i++) {
         	scanner.nextLine();
-            String pat = scanner.nextLine().trim();
-            String txt = scanner.nextLine().trim();
-            System.out.println("#" + i + " " + brF(txt, pat)); 
+            String pattern = scanner.nextLine();
+            String text = scanner.nextLine();
+            System.out.println("#" + i + " " + find(pattern, text));
         }
         scanner.close();
     }
-    public static int brF(String txt, String pat) {
+    public static int find(String pattern, String text) {
     	int count = 0;
-        for (int i = 0; i <= txt.length() - pat.length(); i++) {
+        for (int i = 0; i <= text.length() - pattern.length(); i++) {
         	boolean found = true;
-            for (int j = 0; j < pat.length(); j++) {
-            	if (txt.charAt(i + j) != pat.charAt(j)) {
+            for (int j = 0; j < pattern.length(); j++) {
+            	if (text.charAt(i + j) != pattern.charAt(j)) {
                 	found = false;
                     break;
                 }
