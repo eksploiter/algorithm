09dea8423d0842840393b0eac228e834786a4453
eksploiter <minsu_510@naver.com>
2024-11-05 01:44:49 +0900

    [D2] Title: [S/W 문제해결 기본] 1일차 - 최빈수 구하기, Time: 202 ms, Memory: 38,956 KB -BaekjoonHub

--- SWEA/D2/1204. ［S／W 문제해결 기본］ 1일차 － 최빈수 구하기/［S／W 문제해결 기본］ 1일차 － 최빈수 구하기.java
+++ SWEA/D2/1204. ［S／W 문제해결 기본］ 1일차 － 최빈수 구하기/［S／W 문제해결 기본］ 1일차 － 최빈수 구하기.java
@@ -1,8 +1,8 @@
 import java.util.Scanner;
-// 메서드 사용하지 않고 만들어보기
+
 public class Solution {
 	public static void main(String[] args) {
-    	Scanner scanner = new Scanner(System.in);
+    	Scanner scanner  = new Scanner(System.in);
         int T = scanner.nextInt();
         for (int t = 1; t <= T; t++) {
         	scanner.nextInt();
@@ -10,22 +10,25 @@ public class Solution {
             for (int i = 0; i < 1000; i++) {
             	scores[i] = scanner.nextInt();
             }
-            int[] count = new int[101];
-            for (int i = 0; i < scores.length; i++) {
-            	count[scores[i]]++;
-            }
-            int maxCount = 0;
-            int mode = 0;
-            for (int i = 0; i < count.length; i++) {
-            	if (count[i] > maxCount) {
-                	maxCount = count[i];
-                    mode = i;
-                } else if (count[i] == maxCount && i > mode) {
-                	mode = i;
-                }
-            }
-            System.out.println("#" + t + " " + mode);
+            System.out.println("#" + t + " " + findMode(scores));
         }
         scanner.close();
+    }
+    public static int findMode(int[] scores) {
+    	int[] count = new int[101];
+        for (int i = 0; i < scores.length; i++) {
+        	count[scores[i]]++;
+        }
+        int maxCount = 0; 
+        int mode = 0;
+        for (int i = 0; i < count.length; i++) {
+        	if (count[i] > maxCount) {
+            	maxCount = count[i];
+                mode = i;
+            } else if (count[i] == maxCount && i > mode) {
+            	mode = i;
+            }
+        }
+        return mode;
     }
 }
