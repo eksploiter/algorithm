3d6a468dfc3d10ec8f6cdcc1096e3c3242eac078
eksploiter <minsu_510@naver.com>
2024-11-04 18:27:02 +0900

    [D3] Title: [S/W 문제해결 기본] 3일차 - String, Time: 145 ms, Memory: 29,196 KB -BaekjoonHub

--- SWEA/D3/1213. ［S／W 문제해결 기본］ 3일차 － String/［S／W 문제해결 기본］ 3일차 － String.java
+++ SWEA/D3/1213. ［S／W 문제해결 기본］ 3일차 － String/［S／W 문제해결 기본］ 3일차 － String.java
@@ -1,31 +1,28 @@
 import java.util.Scanner;
-
+// 메서드를 사용하지 않고 풀어보기
 public class Solution {
 	public static void main(String[] args) {
     	Scanner scanner = new Scanner(System.in);
         int T = 10;
-        for (int i = 1; i <= 10; i++) {
-        	scanner.nextLine();
-            String pattern = scanner.nextLine();
-            String text = scanner.nextLine();
-            System.out.println("#" + i + " " + find(pattern, text));
-        }
-        scanner.close();
-    }
-    public static int find(String pattern, String text) {
-    	int count = 0;
-        for (int i = 0; i <= text.length() - pattern.length(); i++) {
-        	boolean found = true;
-            for (int j = 0; j < pattern.length(); j++) {
-            	if (text.charAt(i + j) != pattern.charAt(j)) {
-                	found = false;
-                    break;
+        for (int t = 1; t <= 10; t++) {
+        	scanner.nextLine(); // 문자열 개행 필수
+            String pattern = scanner.nextLine().trim();
+            String text = scanner.nextLine().trim();
+            int count = 0;
+            for (int i = 0; i <= text.length() - pattern.length(); i++) {
+            	boolean found = true;
+                for (int j = 0; j < pattern.length(); j++) {
+                	if (text.charAt(i + j) != pattern.charAt(j)) {
+                    	found = false;
+                        break;
+                    } 
+                }
+                if (found) {
+                	count++;
                 }
             }
-            if (found) {
-            	count++;
-            }
+            System.out.println("#" + t + " " + count);
         }
-        return count;
+        scanner.close();
     }
 }
