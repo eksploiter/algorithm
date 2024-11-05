4dcc51f5246085037788e3aa4531f429474b9d23
eksploiter <minsu_510@naver.com>
2024-11-05 12:20:08 +0900

    [D3] Title: [S/W 문제해결 기본] 5일차 - GNS, Time: 304 ms, Memory: 73,280 KB -BaekjoonHub

--- SWEA/D3/1221. ［S／W 문제해결 기본］ 5일차 － GNS/［S／W 문제해결 기본］ 5일차 － GNS.java
+++ SWEA/D3/1221. ［S／W 문제해결 기본］ 5일차 － GNS/［S／W 문제해결 기본］ 5일차 － GNS.java
@@ -1,36 +1,37 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Scanner;
 
 public class Solution {
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         
-        // 숫자 이름과 숫자를 매핑하기 위한 배열 및 맵
+        // 숫자 이름과 숫자를 매핑하기 위한 배열
         String[] numberWords = {"ZRO", "ONE", "TWO", "THR", "FOR", "FIV", "SIX", "SVN", "EGT", "NIN"};
-        Map<String, Integer> wordToNumber = new HashMap<>();
-        for (int i = 0; i < numberWords.length; i++) {
-            wordToNumber.put(numberWords[i], i);
-        }
+        
+        int testCases = scanner.nextInt();
+        scanner.nextLine(); // 개행 문자 소비
 
-        int testCases = Integer.parseInt(br.readLine().trim());
         StringBuilder result = new StringBuilder();
 
         for (int t = 1; t <= testCases; t++) {
-            String[] header = br.readLine().split(" ");
-            int wordCount = Integer.parseInt(header[1]);
+            // 테스트 케이스의 헤더 읽기
+            String header = scanner.nextLine();
+            int wordCount = Integer.parseInt(header.split(" ")[1]);
 
             // 각 단어의 개수를 저장하는 배열
             int[] count = new int[10];
 
-            // 입력 데이터를 읽어서 단어의 빈도를 계산
-            String[] words = br.readLine().split(" ");
-            for (String word : words) {
-                count[wordToNumber.get(word)]++;
+            // 단어의 빈도를 카운팅
+            for (int i = 0; i < wordCount; i++) {
+                String word = scanner.next();
+                for (int j = 0; j < numberWords.length; j++) {
+                    if (numberWords[j].equals(word)) {
+                        count[j]++;
+                        break;
+                    }
+                }
             }
+            scanner.nextLine(); // 개행 문자 소비
 
             // 테스트 케이스 결과를 저장
             result.append("#").append(t).append("\n");
@@ -41,8 +42,9 @@ public class Solution {
             }
             result.append("\n");
         }
-
+        
         // 최종 결과 출력
         System.out.print(result.toString());
+        scanner.close();
     }
 }
