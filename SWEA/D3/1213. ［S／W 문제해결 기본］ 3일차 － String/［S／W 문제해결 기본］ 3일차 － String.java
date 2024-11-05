0999b5f4e7b3d8e8c4a9529964287acaf2f22d44
eksploiter <minsu_510@naver.com>
2024-11-06 01:55:39 +0900

    [D3] Title: [S/W 문제해결 기본] 3일차 - String, Time: 158 ms, Memory: 21,768 KB -BaekjoonHub

--- SWEA/D3/1213. ［S／W 문제해결 기본］ 3일차 － String/［S／W 문제해결 기본］ 3일차 － String.java
+++ SWEA/D3/1213. ［S／W 문제해결 기본］ 3일차 － String/［S／W 문제해결 기본］ 3일차 － String.java
@@ -3,29 +3,25 @@ import java.util.Scanner;
 public class Solution {
 	public static void main(String[] args) {
     	Scanner scanner = new Scanner(System.in);
-        int T = 10;
-        for (int i = 1; i <= T; i++) {
+        for (int t = 1; t <= 10; t++) {
         	scanner.nextLine();
             String pattern = scanner.nextLine().trim();
             String text = scanner.nextLine().trim();
-            System.out.println("#" + i + " " + find(pattern, text));
-        }
-        scanner.close();
-    }
-    public static int find(String pattern, String text) {
-    	int count = 0;
-        for (int i = 0; i <= text.length() - pattern.length(); i++) {
-        	boolean found = true;
-            for (int j = 0; j < pattern.length(); j++) {
-            	if (text.charAt(i + j) != pattern.charAt(j)) {
-                	found = false;
-                    break;
+            int count = 0;
+            for (int i = 0; i <= text.length() - pattern.length(); i++) {
+            	boolean found = true;
+                for (int j = 0; j < pattern.length(); j++) {
+                	if (text.charAt(i + j) != pattern.charAt(j)) {
+                    	found = false;
+                        break;
+                    }
+                }
+                if (found) {
+                	count++;
                 }
             }
-            if (found) {
-            	count++;
-            }
+            System.out.println("#" + t + " " + count);
         }
-        return count;
+        scanner.close();
     }
 }
