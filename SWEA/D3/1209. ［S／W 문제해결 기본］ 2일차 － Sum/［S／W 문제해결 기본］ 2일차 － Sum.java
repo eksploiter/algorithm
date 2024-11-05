356bfb7e7ca68608a2ebee98f55915c99e42545e
eksploiter <minsu_510@naver.com>
2024-11-06 01:13:54 +0900

    [D3] Title: [S/W 문제해결 기본] 2일차 - Sum, Time: 363 ms, Memory: 111,064 KB -BaekjoonHub

--- SWEA/D3/1209. ［S／W 문제해결 기본］ 2일차 － Sum/［S／W 문제해결 기본］ 2일차 － Sum.java
+++ SWEA/D3/1209. ［S／W 문제해결 기본］ 2일차 － Sum/［S／W 문제해결 기본］ 2일차 － Sum.java
@@ -1,46 +1,37 @@
 import java.util.Scanner;
 
-class Solution {
-    public static void main(String args[]) throws Exception {
-        Scanner scanner = new Scanner(System.in);
-        
-        for (int t = 1; t <= 10; t++) {
-            int testCaseNum = scanner.nextInt(); // 테스트 케이스 번호 입력
-            
-            int[][] matrix = new int[100][100];
-            
-            // 100x100 배열 입력 받기
+public class Solution {
+	public static void main(String[] args) {
+    	Scanner scanner  = new Scanner(System.in);
+        int T = 10;
+        for (int t = 1; t <= T; t++) {
+        	scanner.nextInt();
+            int[][] arr = new int[100][100];
             for (int i = 0; i < 100; i++) {
-                for (int j = 0; j < 100; j++) {
-                    matrix[i][j] = scanner.nextInt();
+            	for (int j = 0; j < 100; j++) {
+                	arr[i][j] = scanner.nextInt();
                 }
             }
-            
             int maxSum = 0;
-            
-            // 각 행의 합과 각 열의 합을 동시에 계산
+            // 각각의 행과 열의 합 계산 시 주의
             for (int i = 0; i < 100; i++) {
-                int rowSum = 0;
                 int colSum = 0;
-                for (int j = 0; j < 100; j++) {
-                    rowSum += matrix[i][j]; // 행의 합
-                    colSum += matrix[j][i]; // 열의 합
-                }
-                maxSum = Math.max(maxSum, Math.max(rowSum, colSum));
+            	int rowSum = 0;
+            	for (int j = 0; j < 100; j++) {
+                	rowSum += arr[i][j];
+                    colSum += arr[j][i];
+                } 
+                maxSum = Math.max(maxSum, (Math.max(rowSum, colSum)));
             }
-            
-            // 대각선의 합 계산
             int diagonal1Sum = 0;
             int diagonal2Sum = 0;
             for (int i = 0; i < 100; i++) {
-                diagonal1Sum += matrix[i][i];
-                diagonal2Sum += matrix[i][99 - i];
+                diagonal1Sum += arr[i][i];
+                diagonal2Sum += arr[i][99 - i];
             }
-            maxSum = Math.max(maxSum, Math.max(diagonal1Sum, diagonal2Sum));
-            
-            // 결과 출력
-            System.out.println("#" + testCaseNum + " " + maxSum);
-        }
+            maxSum = Math.max(maxSum, (Math.max(diagonal1Sum, diagonal2Sum)));
+            System.out.println("#" + t + " " + maxSum);
+        } 
         scanner.close();
     }
 }
