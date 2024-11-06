2471e8c0793c9e95adbf61b7185ff14a90b28a11
eksploiter <minsu_510@naver.com>
2024-11-06 22:51:59 +0900

    [D3] Title: [S/W 문제해결 기본] 2일차 - Sum, Time: 451 ms, Memory: 104,352 KB -BaekjoonHub

--- SWEA/D3/1209. ［S／W 문제해결 기본］ 2일차 － Sum/［S／W 문제해결 기본］ 2일차 － Sum.java
+++ SWEA/D3/1209. ［S／W 문제해결 기본］ 2일차 － Sum/［S／W 문제해결 기본］ 2일차 － Sum.java
@@ -2,9 +2,8 @@ import java.util.Scanner;
 
 public class Solution {
 	public static void main(String[] args) {
-    	Scanner scanner  = new Scanner(System.in);
-        int T = 10;
-        for (int t = 1; t <= T; t++) {
+    	Scanner scanner = new Scanner(System.in);
+        for (int t = 1; t <= 10; t++) {
         	scanner.nextInt();
             int[][] arr = new int[100][100];
             for (int i = 0; i < 100; i++) {
@@ -12,26 +11,28 @@ public class Solution {
                 	arr[i][j] = scanner.nextInt();
                 }
             }
-            int maxSum = 0;
-            // 각각의 행과 열의 합 계산 시 주의
-            for (int i = 0; i < 100; i++) {
-                int colSum = 0;
-            	int rowSum = 0;
-            	for (int j = 0; j < 100; j++) {
-                	rowSum += arr[i][j];
-                    colSum += arr[j][i];
-                } 
-                maxSum = Math.max(maxSum, (Math.max(rowSum, colSum)));
-            }
-            int diagonal1Sum = 0;
-            int diagonal2Sum = 0;
-            for (int i = 0; i < 100; i++) {
-                diagonal1Sum += arr[i][i];
-                diagonal2Sum += arr[i][99 - i];
-            }
-            maxSum = Math.max(maxSum, (Math.max(diagonal1Sum, diagonal2Sum)));
-            System.out.println("#" + t + " " + maxSum);
-        } 
+            System.out.println("#" + t + " " + find(arr));
+        }
         scanner.close();
+    }
+    public static int find(int[][] arr) {
+    	int maxSum = 0;
+        for (int i = 0; i < 100; i++) {
+        	int rowSum = 0;
+            int colSum = 0;
+            for (int j = 0; j < 100; j++) {
+            	rowSum += arr[i][j];
+                colSum += arr[j][i];
+            }
+            maxSum = Math.max(maxSum, Math.max(rowSum, colSum));
+        }
+        int diag1 = 0;
+        int diag2 = 0;
+        for (int i = 0; i < 100; i++) {
+        	diag1 += arr[i][i];
+            diag2 += arr[i][99 - i];
+        }
+        maxSum = Math.max(maxSum, Math.max(diag1, diag2));
+        return maxSum;
     }
 }
