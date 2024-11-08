4e55d0c7f9e24eb6069aa1b8d3ef3e1c87a0d0c5
eksploiter <minsu_510@naver.com>
2024-11-08 23:05:26 +0900

    [D3] Title: 농작물 수확하기, Time: 181 ms, Memory: 28,700 KB -BaekjoonHub

---
+++ SWEA/D3/2805. 농작물 수확하기/README.md
@@ -0,0 +1,15 @@
+# [D3] 농작물 수확하기 - 2805 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV7GLXqKAWYDFAXB) 
+
+### 성능 요약
+
+메모리: 28,700 KB, 시간: 181 ms, 코드길이: 1,074 Bytes
+
+### 제출 일자
+
+2024-11-08 23:05
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D3/2805. 농작물 수확하기/농작물 수확하기.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int T = scanner.nextInt();
+        for (int t = 1; t <= T; t++) {
+            int N = scanner.nextInt();
+            int[][] farm = new int[N][N];
+            for (int i = 0; i < N; i++) {
+                String line = scanner.next();
+                for (int j = 0; j < N; j++) {
+                    farm[i][j] = line.charAt(j) - '0';
+                }
+            }
+            System.out.println("#" + t + " " + value(farm));
+        }
+        scanner.close();
+    }
+    
+    public static int value(int[][] farm) {
+        int n = farm.length;
+        int radius = (n - 1) / 2;
+        int center = n / 2;
+        int sum = 0;
+        for (int i = -radius; i <= radius; i++) {
+            for (int j = -radius; j <= radius; j++) {
+                if (Math.abs(i) + Math.abs(j) <= radius) {
+                    sum += farm[center + i][center + j];
+                }
+            }
+        }
+        return sum;
+    }
+}
