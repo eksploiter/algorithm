251b784cdb40c2ea5913c4bbbe8fe06bc9ee336c
eksploiter <minsu_510@naver.com>
2024-11-09 17:36:51 +0900

    [D2] Title: [S/W 문제해결 기본] 1일차 - 최빈수 구하기, Time: 191 ms, Memory: 40,868 KB -BaekjoonHub

--- SWEA/D2/1204. ［S／W 문제해결 기본］ 1일차 － 최빈수 구하기/［S／W 문제해결 기본］ 1일차 － 최빈수 구하기.java
+++ SWEA/D2/1204. ［S／W 문제해결 기본］ 1일차 － 최빈수 구하기/［S／W 문제해결 기본］ 1일차 － 최빈수 구하기.java
@@ -4,12 +4,12 @@ public class Solution {
 	public static void main(String[] args) {
     	Scanner scanner = new Scanner(System.in);
         int T = scanner.nextInt();
-        for (int t = 1; t <= T; t++) {
+        for (int t = 1; t <= 10; t++) {
         	scanner.nextInt();
             int[] scores = new int[1000];
             for (int i = 0; i < 1000; i++) {
             	scores[i] = scanner.nextInt();
-            }
+            } 
             System.out.println("#" + t + " " + find(scores));
         }
         scanner.close();
@@ -21,7 +21,7 @@ public class Solution {
         }
         int maxCount = 0;
         int score = 0;
-        for (int i = 0; i < count.length; i++) {
+        for (int i = 0; i < 101; i++) {
         	if (count[i] > maxCount) {
             	maxCount = count[i];
                 score = i;
