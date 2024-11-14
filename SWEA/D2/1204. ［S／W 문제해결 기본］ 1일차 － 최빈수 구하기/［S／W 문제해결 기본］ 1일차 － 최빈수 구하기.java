d78019ec2cbf8635409861f81fefdc78576cf094
eksploiter <minsu_510@naver.com>
2024-11-14 17:44:41 +0900

    [D2] Title: [S/W 문제해결 기본] 1일차 - 최빈수 구하기, Time: 199 ms, Memory: 38,248 KB -BaekjoonHub

--- SWEA/D2/1204. ［S／W 문제해결 기본］ 1일차 － 최빈수 구하기/［S／W 문제해결 기본］ 1일차 － 최빈수 구하기.java
+++ SWEA/D2/1204. ［S／W 문제해결 기본］ 1일차 － 최빈수 구하기/［S／W 문제해결 기본］ 1일차 － 최빈수 구하기.java
@@ -7,23 +7,28 @@ public class Solution {
         for (int t = 1; t <= T; t++) {
         	scanner.nextInt();
             int[] arr = new int[1000];
-            int[] count = new int[101]; 
             for (int i = 0; i < 1000; i++) {
             	arr[i] = scanner.nextInt();
-                count[arr[i]]++;
             }
-            int maxCount = 0; 
-            int score = 0;
-            for (int i = 0; i < count.length; i++) {
-            	if (count[i] > maxCount) {
-                	maxCount = count[i];
-                    score = i;
-                } else if (count[i] == maxCount && i > score) {
-                	score = i;
-                }
-            }
-            System.out.println("#" + t + " " + score);
+            System.out.println("#" + t + " " + find(arr));
         }
         scanner.close();
+    }
+    public static int find(int[] arr) {
+    	int[] count = new int[101];
+        for (int i = 0; i < 1000; i++) {
+        	count[arr[i]]++;
+        }
+        int maxCount = 0;
+        int score = 0;
+        for (int i = 0; i < count.length; i++) {
+        	if (count[i] > maxCount) {
+            	maxCount = count[i];
+                score = i;
+            } else if (count[i] == maxCount && i > score) {
+            	score = i;
+            }
+        }
+        return score;
     }
 }
