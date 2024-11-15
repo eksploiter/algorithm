7bdfce02b1990cbb043f9572f681924aeb4678e7
eksploiter <minsu_510@naver.com>
2024-11-15 13:54:11 +0900

    [D3] Title: [S/W 문제해결 기본] 4일차 - 거듭 제곱, Time: 126 ms, Memory: 20,392 KB -BaekjoonHub

--- SWEA/D3/1217. ［S／W 문제해결 기본］ 4일차 － 거듭 제곱/［S／W 문제해결 기본］ 4일차 － 거듭 제곱.java
+++ SWEA/D3/1217. ［S／W 문제해결 기본］ 4일차 － 거듭 제곱/［S／W 문제해결 기본］ 4일차 － 거듭 제곱.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Solution {
 	public static void main(String[] args) {
     	Scanner scanner = new Scanner(System.in);
-        for (int t =1; t <= 10; t++) {
+        for (int t = 1; t <= 10; t++) {
         	scanner.nextInt();
             int N = scanner.nextInt();
             int M = scanner.nextInt();
