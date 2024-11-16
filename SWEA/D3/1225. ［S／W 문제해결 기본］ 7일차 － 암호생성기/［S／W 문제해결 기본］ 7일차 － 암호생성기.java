08f9ef9cf4182a8b93a695c319971ede66954b32
eksploiter <minsu_510@naver.com>
2024-11-16 15:07:21 +0900

    [D3] Title: [S/W 문제해결 기본] 7일차 - 암호생성기, Time: 154 ms, Memory: 23,152 KB -BaekjoonHub

--- SWEA/D3/1225. ［S／W 문제해결 기본］ 7일차 － 암호생성기/［S／W 문제해결 기본］ 7일차 － 암호생성기.java
+++ SWEA/D3/1225. ［S／W 문제해결 기본］ 7일차 － 암호생성기/［S／W 문제해결 기본］ 7일차 － 암호생성기.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Solution {
-	public static void main(String[] args) {
+	public static void main(String[] ags) {
     	Scanner scanner = new Scanner(System.in);
         for (int t = 1; t <= 10; t++) {
         	scanner.nextInt();
