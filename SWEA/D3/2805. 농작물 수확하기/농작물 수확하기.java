a9b60b9e75639461b241ddc0ea895dfea5aa4339
Kim Minsoo <minsu_510@naver.com>
2024-11-22 10:40:33 +0900

    [D3] Title: 농작물 수확하기, Time: 166 ms, Memory: 30,420 KB -BaekjoonHub

--- SWEA/D3/2805. 농작물 수확하기/농작물 수확하기.java
+++ SWEA/D3/2805. 농작물 수확하기/농작물 수확하기.java
@@ -1,32 +1,31 @@
 import java.util.Scanner;
 
 public class Solution {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+	public static void main(String[] args) {
+    	Scanner scanner = new Scanner(System.in);
         int T = scanner.nextInt();
         for (int t = 1; t <= T; t++) {
-            int N = scanner.nextInt();
+        	int N = scanner.nextInt();
             int[][] farm = new int[N][N];
             for (int i = 0; i < N; i++) {
-                String line = scanner.next();
+            	String line = scanner.next();
                 for (int j = 0; j < N; j++) {
-                    farm[i][j] = line.charAt(j) - '0';
+                	farm[i][j] = line.charAt(j) - '0';
                 }
             }
-            System.out.println("#" + t + " " + value(farm));
+            System.out.println("#" + t + " " + find(N, farm));
         }
         scanner.close();
     }
-    
-    public static int value(int[][] farm) {
-        int n = farm.length;
-        int radius = (n - 1) / 2;
-        int center = n / 2;
+    public static int find(int N, int[][] farm) {
+    	
+        int radius = (N - 1) / 2;
+        int center = N / 2;
         int sum = 0;
         for (int i = -radius; i <= radius; i++) {
-            for (int j = -radius; j <= radius; j++) {
-                if (Math.abs(i) + Math.abs(j) <= radius) {
-                    sum += farm[center + i][center + j];
+        	for (int j = -radius; j <= radius; j++) {
+            	if (Math.abs(i) + Math.abs(j) <= radius) {
+                	sum += farm[center + i][center + j];
                 }
             }
         }
