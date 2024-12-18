d8d784d34e78ac18e6846c5718cb8b39a0b3b051
Kim Minsoo <minsu_510@naver.com>
2024-12-18 17:17:35 +0900

    [D3] Title: [S/W 문제해결 기본] 3일차 - String, Time: 150 ms, Memory: 22,208 KB -BaekjoonHub

--- SWEA/D3/1213. ［S／W 문제해결 기본］ 3일차 － String/［S／W 문제해결 기본］ 3일차 － String.java
+++ SWEA/D3/1213. ［S／W 문제해결 기본］ 3일차 － String/［S／W 문제해결 기본］ 3일차 － String.java
@@ -7,21 +7,24 @@ public class Solution {
         	scanner.nextLine();
             String pattern = scanner.nextLine().trim();
             String text = scanner.nextLine().trim();
-            int count = 0;
-            for (int i = 0; i <= text.length() - pattern.length(); i++) {
-            	boolean found = true;
-                for (int j = 0; j < pattern.length(); j++) {
-                	if (text.charAt(i + j) != pattern.charAt(j)) {
-                    	found = false;
-                        break;
-                    }
-                }
-                if (found) {
-                	count++;
-                }
-            }
-            System.out.println("#" + t + " " + count); 
+            System.out.println("#" + t + " " + find(pattern, text));
         }
         scanner.close();
+    }
+    public static int find(String pattern, String text) {
+    	int count = 0;
+        for (int i = 0; i <= text.length() - pattern.length(); i++) {
+        	boolean found = true;
+            for (int j = 0; j < pattern.length(); j++) {
+            	if (text.charAt(i + j) != pattern.charAt(j)) {
+                	found = false;
+                    break;
+                }
+            }
+            if (found) {
+            	count++;
+            }
+        }
+        return count;
     }
 }
