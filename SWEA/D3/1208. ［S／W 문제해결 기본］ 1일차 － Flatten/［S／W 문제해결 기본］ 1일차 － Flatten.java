21b289acd50451e967c8d134eeeac9289aa2ee6f
Kim Minsoo <minsu_510@naver.com>
2025-01-03 01:04:41 +0900

    [D3] Title: [S/W 문제해결 기본] 1일차 - Flatten, Time: 172 ms, Memory: 25,248 KB -BaekjoonHub

--- SWEA/D3/1208. ［S／W 문제해결 기본］ 1일차 － Flatten/［S／W 문제해결 기본］ 1일차 － Flatten.java
+++ SWEA/D3/1208. ［S／W 문제해결 기본］ 1일차 － Flatten/［S／W 문제해결 기본］ 1일차 － Flatten.java
@@ -2,22 +2,22 @@ import java.util.Scanner;
 
 public class Solution {
 	public static void main(String[] args) {
-    	Scanner scanner = new Scanner(System.in);
-        for (int t = 1; t <= 10; t++) {
-        	int N = scanner.nextInt();
-            int[] arr = new int[100];
-            for (int i = 0; i < 100; i++) {
-            	arr[i] = scanner.nextInt();
-            }
-            System.out.println("#" + t + " " + find(N, arr));
+        Scanner scanner = new Scanner(System.in);
+    	for (int t = 1; t <= 10; t++) {
+    		int N = scanner.nextInt();
+        	int[] arr = new int[100];
+       		for (int i = 0; i < 100; i++) {
+        		arr[i] = scanner.nextInt();
+        	}
+        	System.out.println("#" + t + " " + flatten(N, arr));
         }
         scanner.close();
     }
-    public static int find(int N, int[] arr) {
+    public static int flatten(int N, int[] arr) {
     	for (int i = 0; i < N; i++) {
         	int top = 0;
             int bottom = 0;
-            for (int j = 1; j < arr.length; j++) {
+            for (int j = 0; j < arr.length; j++) {
             	if (arr[j] > arr[top]) {
                 	top = j;
                 }
@@ -28,7 +28,7 @@ public class Solution {
             arr[top] -= 1;
             arr[bottom] += 1;
         }
-        int top = 0;
+        int top = 0; 
         int bottom = 0;
         for (int i = 0; i < arr.length; i++) {
         	if (arr[i] > arr[top]) {
