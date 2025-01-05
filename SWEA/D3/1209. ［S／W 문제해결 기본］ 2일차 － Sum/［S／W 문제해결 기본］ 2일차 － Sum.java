6aae3c434857736c208ff1d9b31ae6f6df6087b9
Kim Minsoo <minsu_510@naver.com>
2025-01-06 01:24:40 +0900

    [D3] Title: [S/W 문제해결 기본] 2일차 - Sum, Time: 322 ms, Memory: 97,784 KB -BaekjoonHub

--- SWEA/D3/1209. ［S／W 문제해결 기본］ 2일차 － Sum/［S／W 문제해결 기본］ 2일차 － Sum.java
+++ SWEA/D3/1209. ［S／W 문제해결 기본］ 2일차 － Sum/［S／W 문제해결 기본］ 2일차 － Sum.java
@@ -11,25 +11,28 @@ public class Solution {
                 	arr[i][j] = scanner.nextInt();
                 }
             }
-            int maxSum = 0;
-            for (int i = 0; i < 100; i++) {
-            	int rowSum = 0;
-                int colSum = 0;
-                for (int j = 0; j < 100; j++) {
-                	rowSum += arr[i][j];
-                    colSum += arr[j][i];
-                }
-                maxSum = Math.max(maxSum, Math.max(rowSum, colSum));
-            }
-            int diag1 = 0;
-            int diag2 = 0;
-            for (int i = 0; i < 100; i++) {
-            	diag1 += arr[i][i];
-                diag2 += arr[i][99 - i];
-            }
-            maxSum = Math.max(maxSum, Math.max(diag1, diag2));
-            System.out.println("#" + t + " " + maxSum); 
+            System.out.println("#" + t + " " + find(arr));
         }
         scanner.close();
+    }
+    public static int find(int[][] arr) {
+    	int maxSum = 0;
+        for (int i = 0; i < 100; i++) {
+        	int rowSum = 0;
+            int colSum = 0;
+            for (int j = 0; j < 100; j++) {
+            	rowSum += arr[i][j];
+                colSum += arr[j][i];
+            }
+            maxSum = Math.max(maxSum, Math.max(rowSum, colSum));
+        }
+        int diag1 = 0;
+        int diag2 = 0;
+        for (int i = 0; i < 100; i++) {
+        	diag1 += arr[i][i];
+            diag2 += arr[i][99 - i];
+        }
+        maxSum = Math.max(maxSum, Math.max(diag1, diag2));
+        return maxSum;
     }
 }
