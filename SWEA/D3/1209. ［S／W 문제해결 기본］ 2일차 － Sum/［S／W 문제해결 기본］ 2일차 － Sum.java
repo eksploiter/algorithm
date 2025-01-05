fb14a42ca433bb905d33c204b9c790d0383e1e29
Kim Minsoo <minsu_510@naver.com>
2025-01-06 01:57:28 +0900

    [D3] Title: [S/W 문제해결 기본] 2일차 - Sum, Time: 377 ms, Memory: 104,564 KB -BaekjoonHub

--- SWEA/D3/1209. ［S／W 문제해결 기본］ 2일차 － Sum/［S／W 문제해결 기본］ 2일차 － Sum.java
+++ SWEA/D3/1209. ［S／W 문제해결 기본］ 2일차 － Sum/［S／W 문제해결 기본］ 2일차 － Sum.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Solution {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception{
     	Scanner scanner = new Scanner(System.in);
         for (int t = 1; t <= 10; t++) {
         	scanner.nextInt();
