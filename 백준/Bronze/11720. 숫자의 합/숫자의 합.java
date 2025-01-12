4657e334b3e29d1be5f1f5d0f53ad71fad54da33
Kim Minsoo <minsu_510@naver.com>
2025-01-12 17:53:55 +0900

    [Bronze IV] Title: 숫자의 합, Time: 168 ms, Memory: 17712 KB -BaekjoonHub

--- 백준/Bronze/11720. 숫자의 합/숫자의 합.java
+++ 백준/Bronze/11720. 숫자의 합/숫자의 합.java
@@ -3,18 +3,15 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        int N = scanner.nextInt();  // 숫자의 개수 입력
-        String numbers = scanner.next();  // 공백 없이 이어진 숫자 문자열 입력
-
-        int total = 0;
-
-        // 문자열을 순회하며 각 문자를 숫자로 변환하고 합산
+        int N = scanner.nextInt();
+        String sNum = scanner.next();
+        char[] cNum = sNum.toCharArray();
+        int sum = 0;
         for (int i = 0; i < N; i++) {
-            total += numbers.charAt(i) - '0';  // char를 int로 변환
+            sum += cNum[i] - '0';
+            // sum += cNum - 48과 동일하다.
         }
-
-        System.out.println(total);  // 합계 출력
+        System.out.println(sum);
         scanner.close();
     }
 }
