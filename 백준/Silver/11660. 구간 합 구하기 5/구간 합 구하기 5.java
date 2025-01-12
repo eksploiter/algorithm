b4c0f1e32d39417e3066958df2d369e21e50641b
Kim Minsoo <minsu_510@naver.com>
2025-01-12 21:24:22 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 1340 ms, Memory: 128976 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -11,7 +11,7 @@ public class Main {
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
         
-        int A[][] = new int[N + 1][N + 1];
+        int[][] A = new int[N + 1][N + 1];
         for (int i = 1 ; i <= N; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 1; j <= N; j++) {
@@ -19,7 +19,7 @@ public class Main {
             }
         }
         
-        int D[][] = new int[N + 1][N + 1];
+        int[][] D = new int[N + 1][N + 1];
         for (int i = 1; i <= N; i++) {
             for (int j = 1; j <= N; j++) {
                 D[i][j] = D[i][j - 1] + D[i - 1][j] - D[i - 1][j - 1] + A[i][j];
