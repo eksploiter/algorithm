8e1442d78e1bc3af6dab6ead028c74dae77f6d0a
Kim Minsoo <minsu_510@naver.com>
2025-01-12 18:33:07 +0900

    [Bronze I] Title: 평균, Time: 212 ms, Memory: 18300 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -3,26 +3,20 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         int N = scanner.nextInt();
         int[] scores = new int[N];
-
         for (int i = 0; i < N; i++) {
             scores[i] = scanner.nextInt();
         }
-
-        int M = scores[0];
-        for (int i = 1; i < N; i++) {
-            if (scores[i] > M) {
-                M = scores[i];
-            }
-        }
-
-        double total = 0;
+        double highScore = 0;
+        double sum = 0;
         for (int i = 0; i < N; i++) {
-            total += ((double) scores[i] / M) * 100;
+            if (scores[i] > highScore) {
+                highScore = scores[i];
+            }
+            sum += scores[i];
         }
-        double average =  total / N;
-        System.out.println(average);
+        System.out.println(sum * 100 / highScore / N);
+        scanner.close();
     }
 }
