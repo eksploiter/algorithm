484a02019bbc179c79ab933f2366dbf86e40f05b
Kim Minsoo <minsu_510@naver.com>
2025-02-03 00:22:30 +0900

    [Bronze III] Title: 별 찍기 - 4, Time: 284 ms, Memory: 20716 KB -BaekjoonHub

---
+++ 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k < N - i; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        scanner.close();
+    }
+}
