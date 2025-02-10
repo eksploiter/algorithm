600180224da4cf21b41e3f9372350089fbfdaaa3
Kim Minsoo <minsu_510@naver.com>
2025-02-11 01:45:46 +0900

    [Silver IV] Title: 수 찾기, Time: 540 ms, Memory: 48844 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -6,25 +6,21 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         
-        // 첫 번째 정수 배열 입력
         int N = Integer.parseInt(br.readLine());
         HashSet<Integer> set = new HashSet<>();
-
+        
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
             set.add(Integer.parseInt(st.nextToken()));
         }
-
-        // 두 번째 정수 배열 입력
+        
         int M = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine());
-
-        // 결과 출력
         for (int i = 0; i < M; i++) {
             int num = Integer.parseInt(st.nextToken());
             sb.append(set.contains(num) ? "1\n" : "0\n");
         }
-
-        System.out.print(sb);
+        
+        System.out.println(sb);
     }
 }
