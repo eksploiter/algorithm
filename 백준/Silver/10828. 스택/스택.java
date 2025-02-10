379eae9256389a5e2d7961ae184b44b36f0bce17
Kim Minsoo <minsu_510@naver.com>
2025-02-10 22:42:20 +0900

    [Silver IV] Title: 스택, Time: 168 ms, Memory: 19808 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.java
+++ 백준/Silver/10828. 스택/스택.java
@@ -6,26 +6,24 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         Stack<Integer> stack = new Stack<>();
-
         int N = Integer.parseInt(br.readLine());
-
+        
         for (int i = 0; i < N; i++) {
-            String command = br.readLine();
-
-            if (command.startsWith("push")) {
-                int x = Integer.parseInt(command.split(" ")[1]);
+            String cmd = br.readLine();
+            
+            if (cmd.startsWith("push")) {
+                int x = Integer.parseInt(cmd.split(" ")[1]);
                 stack.push(x);
-            } else if (command.equals("pop")) {
+            } else if (cmd.equals("pop")) {
                 sb.append(stack.isEmpty() ? "-1" : stack.pop()).append("\n");
-            } else if (command.equals("size")) {
+            } else if (cmd.equals("size")) {
                 sb.append(stack.size()).append("\n");
-            } else if (command.equals("empty")) {
+            } else if (cmd.equals("empty")) {
                 sb.append(stack.isEmpty() ? "1" : "0").append("\n");
-            } else if (command.equals("top")) {
+            } else if (cmd.equals("top")) {
                 sb.append(stack.isEmpty() ? "-1" : stack.peek()).append("\n");
             }
         }
-        
         System.out.println(sb);
     }
 }
