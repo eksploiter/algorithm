c4c770a70550fda519fc9de24c0b15fc996f62bb
Kim Minsoo <minsu_510@naver.com>
2025-02-16 02:28:34 +0900

    [Silver I] Title: 미로 탐색, Time: 112 ms, Memory: 14592 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -2,7 +2,7 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int[][] board;
+    static int[][] miro;
     static int[][] dist;
     static int n, m;
     static int[] dx = {1, 0, -1, 0};
@@ -15,20 +15,21 @@ public class Main {
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
 
-        board = new int[n][m];
+        miro = new int[n][m];
         dist = new int[n][m];
 
         for (int i = 0; i < n; i++) {
-            String line = br.readLine(); // 입력 값에 띄어쓰기가 없다.
+            String line = br.readLine();
             for (int j = 0; j < m; j++) {
-                board[i][j] = line.charAt(j) - '0';
-                dist[i][j] = -1; // 방문하지 않은 곳을 -1로 만든다.
+                miro[i][j] = line.charAt(j) - '0';
+                dist[i][j] = -1;
             }
         }
 
-        bfs(0, 0);
-
+        bfs(0,0);
         System.out.println(dist[n - 1][m - 1] + 1);
+        // 배열이 (0,0) 부터 사작하고 
+        // (0,0)에서 출발을 하기 때문에 시작점도 거리에 포함해야 하므로 +1을 해야한다.
     }
 
     public static void bfs(int startX, int startY) {
@@ -46,8 +47,7 @@ public class Main {
                 int ny = y + dy[dir];
 
                 if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
-                // 벽이거나 이미 방문 했거나 
-                if (board[nx][ny] != 1 || dist[nx][ny] >= 0) continue;
+                if (dist[nx][ny] >= 0 || miro[nx][ny] != 1) continue;
 
                 dist[nx][ny] = dist[x][y] + 1;
                 queue.add(new int[]{nx, ny});
