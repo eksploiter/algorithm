87dcd06179e377f20865ae57c23e4a9136c82875
Kim Minsoo <minsu_510@naver.com>
2025-02-17 00:11:53 +0900

    [Silver I] Title: 미로 탐색, Time: 108 ms, Memory: 14796 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -4,7 +4,7 @@ import java.util.*;
 public class Main {
     static int[][] miro;
     static int[][] dist;
-    static int n, m;
+    static int N, M;
     static int[] dx = {1, 0, -1, 0};
     static int[] dy = {0, 1, 0, -1};
 
@@ -12,24 +12,22 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
-        miro = new int[n][m];
-        dist = new int[n][m];
+        miro = new int[N][M];
+        dist = new int[N][M];
 
-        for (int i = 0; i < n; i++) {
-            String line = br.readLine();
-            for (int j = 0; j < m; j++) {
-                miro[i][j] = line.charAt(j) - '0';
+        for (int i = 0; i < N; i++) {
+            String input = br.readLine();
+            for (int j = 0; j < M; j++) {
+                miro[i][j] = input.charAt(j) - '0';
                 dist[i][j] = -1;
             }
         }
 
-        bfs(0,0);
-        System.out.println(dist[n - 1][m - 1] + 1);
-        // 배열이 (0,0) 부터 사작하고 
-        // (0,0)에서 출발을 하기 때문에 시작점도 거리에 포함해야 하므로 +1을 해야한다.
+        bfs(0, 0);
+        System.out.println(dist[N - 1][M - 1] + 1);
     }
 
     public static void bfs(int startX, int startY) {
@@ -37,7 +35,7 @@ public class Main {
         queue.add(new int[]{startX, startY});
         dist[startX][startY] = 0;
 
-        while (!queue.isEmpty()) {
+        while(!queue.isEmpty()) {
             int[] cur = queue.poll();
             int x = cur[0];
             int y = cur[1];
@@ -46,8 +44,8 @@ public class Main {
                 int nx = x + dx[dir];
                 int ny = y + dy[dir];
 
-                if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
-                if (dist[nx][ny] >= 0 || miro[nx][ny] != 1) continue;
+                if (nx < 0 || nx >= N || ny < 0 || ny >= M) continue;
+                if (miro[nx][ny] != 1 || dist[nx][ny] >= 0) continue;
 
                 dist[nx][ny] = dist[x][y] + 1;
                 queue.add(new int[]{nx, ny});
