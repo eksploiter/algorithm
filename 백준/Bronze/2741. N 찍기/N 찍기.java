5f046e2a52b614a483032f8e2c8af80ea49b0c0d
Kim Minsoo <minsu_510@naver.com>
2025-02-17 00:16:15 +0900

    [Bronze V] Title: N 찍기, Time: 804 ms, Memory: 32364 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int N = scanner.nextInt();
+
+        for (int i = 1; i <= N; i++) {
+            System.out.println(i);
+        }
+
+        scanner.close();
+    }
+}
