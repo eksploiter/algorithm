b4e75a428f77b97ce0fd236db6f686188b64f6a9
Kim Minsoo <minsu_510@naver.com>
2025-02-19 21:21:15 +0900

    [Bronze II] Title: 벌집, Time: 172 ms, Memory: 17712 KB -BaekjoonHub

---
+++ 백준/Bronze/2292. 벌집/벌집.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        int count = 1;
+        int range = 2;
+
+        if (N == 1) {
+            System.out.println(1);
+        } else {
+            while (range <= N) {
+                range = range + (count * 6);
+                count++;
+            }
+            System.out.println(count);
+        }
+        scanner.close();
+    }
+}
