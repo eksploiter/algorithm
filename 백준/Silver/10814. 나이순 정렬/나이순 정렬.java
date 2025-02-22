0475a2ca014dfa3ce2bc7e2b5d03153155d9f9ef
Kim Minsoo <minsu_510@naver.com>
2025-02-23 02:11:12 +0900

    [Silver V] Title: 나이순 정렬, Time: 640 ms, Memory: 53956 KB -BaekjoonHub

---
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static class Person {
+        int age;
+        String name;
+
+        Person (int age, String name) {
+            this.age = age;
+            this.name = name;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        Person[] persons = new Person[N];
+
+        for (int i = 0; i < N; i++) {
+            String[] input = br.readLine().split(" ");
+            int age = Integer.parseInt(input[0]);
+            String name = input[1];
+            persons[i] = new Person(age, name);
+        }
+
+        Arrays.sort(persons, new Comparator<Person>() {
+            @Override
+            public int compare(Person o1, Person o2) {
+                return o1.age - o2.age;
+            }
+        });
+
+        StringBuilder sb = new StringBuilder();
+        for (Person p : persons) {
+            sb.append(p.age).append(" ").append(p.name).append("\n");
+        }
+        System.out.print(sb);
+    }
+}
