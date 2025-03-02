945c6c716c6f0a05d897a52c55a494fde7b4c134
Kim Minsoo <minsu_510@naver.com>
2025-03-03 01:13:23 +0900

    [Silver V] Title: 다리 놓기, Time: 108 ms, Memory: 14288 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.java
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.java
@@ -1,36 +1,38 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = null;
 
-    // 조합을 계산하는 함수
-    public static long combination(int m, int n) {
-        if (n == 0 || n == m) {
+        int T = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < T; i++) {
+            st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken());
+            int M = Integer.parseInt(st.nextToken());
+
+            long ans = combination(M, N);
+
+            sb.append(ans).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+
+    static long combination(int M, int N) {
+        if (N == 0 || N == M) {
             return 1;
         }
 
         long result = 1;
 
-        // 조합 공식 적용 C(M, N) = M! / (N! * (M-N)!)
-        for (int i = 1; i <= n; i++) {
-            result = result * (m - i + 1) / i;
+        for (int i = 1; i <= N; i++) {
+            result = result * (M - i + 1) / i;
         }
 
         return result;
-    }
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        int T = sc.nextInt();  // 테스트 케이스의 개수
-
-        for (int t = 0; t < T; t++) {
-            int N = sc.nextInt();
-            int M = sc.nextInt();
-
-            // 조합 계산하여 출력
-            System.out.println(combination(M, N));
-        }
-
-        sc.close();
     }
 }
