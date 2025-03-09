7567db64de376459e1df5687854261879d940b2d
Kim Minsoo <minsu_510@naver.com>
2025-03-10 00:16:47 +0900

    [Bronze IV] Title: 세수정렬, Time: 104 ms, Memory: 14292 KB -BaekjoonHub

---
+++ 백준/Bronze/2752. 세수정렬/세수정렬.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int[] arr = new int[3];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        StringBuilder sb = new StringBuilder();
+        for (int j : arr) {
+            sb.append(j).append(" ");
+        }
+
+        System.out.println(sb);
+    }
+}
