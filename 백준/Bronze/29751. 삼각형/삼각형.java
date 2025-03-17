40ae5733b575be9a88b1dc2a93680e8cc04d648c
Kim Minsoo <minsu_510@naver.com>
2025-03-18 00:31:22 +0900

    [Bronze V] Title: 삼각형, Time: 104 ms, Memory: 14176 KB -BaekjoonHub

---
+++ 백준/Bronze/29751. 삼각형/삼각형.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int W = Integer.parseInt(st.nextToken());
+        int H = Integer.parseInt(st.nextToken());
+
+        double ans = (double) (W * H) / 2;
+
+        System.out.println(ans);
+    }
+}
