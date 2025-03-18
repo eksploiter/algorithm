5f1eca2caf2cec05b6fc778457dd983dacbe90af
Kim Minsoo <minsu_510@naver.com>
2025-03-19 01:11:19 +0900

    [Bronze V] Title: 홍익대학교, Time: 168 ms, Memory: 17628 KB -BaekjoonHub

---
+++ 백준/Bronze/16394. 홍익대학교/홍익대학교.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        int ans = N - 1946;
+        System.out.println(ans);
+    }
+}
