10592ad2ac409fd9e25fa70d416fd5d4e970ec26
Kim Minsoo <minsu_510@naver.com>
2025-03-20 01:28:38 +0900

    [Bronze IV] Title: 기찍 N, Time: 184 ms, Memory: 17904 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.java
@@ -0,0 +1,17 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            int ans = N - i;
+            sb.append(ans).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
