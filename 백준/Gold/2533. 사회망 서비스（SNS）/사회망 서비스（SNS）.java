71006bed6fe6f2e0886cbc1df305729969999986
eksploiter <minsu_510@naver.com>
2025-05-09 09:04:55 +0900

    [Gold III] Title: 사회망 서비스(SNS), Time: 2136 ms, Memory: 427604 KB -BaekjoonHub

---
+++ 백준/Gold/2533. 사회망 서비스（SNS）/사회망 서비스（SNS）.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N;
+    static List<Integer>[] tree;
+    static int[][] dp;
+    static boolean[] visited;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        tree = new ArrayList[N + 1];
+        dp = new int[N + 1][2];
+        visited = new boolean[N + 1];
+
+        for (int i = 1; i <= N; i++) {
+            tree[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < N - 1; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            tree[u].add(v);
+            tree[v].add(u);
+        }
+
+        dfs(1);
+
+        System.out.println(Math.min(dp[1][0], dp[1][1]));
+    }
+
+    static void dfs(int node) {
+        visited[node] = true;
+        dp[node][0] = 0;
+        dp[node][1] = 1;
+
+        for (int child : tree[node]) {
+            if (!visited[child]) {
+                dfs(child);
+                dp[node][0] += dp[child][1];
+                dp[node][1] += Math.min(dp[child][0], dp[child][1]);
+            }
+        }
+    }
+}
