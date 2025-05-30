9cd008da8ab40b5c498f423c2200ef6a5724b78a
Kim Minsoo <minsu_510@naver.com>
2025-05-30 16:24:25 +0900

    [Bronze IV] Title: 주사위, Time: 116 ms, Memory: 14488 KB -BaekjoonHub

---
+++ 백준/Bronze/9295. 주사위/주사위.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int T = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= T; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int sum = a + b;
+            sb.append("Case ").append(i).append(": ").append(sum).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
