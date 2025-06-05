83e2256ee90a1291224330b0de447a8df0d66bae
Minsoo Kim <minsu_510@naver.com>
2025-06-06 02:41:30 +0900

    [Bronze V] Title: 身長 (Height), Time: 100 ms, Memory: 14056 KB -BaekjoonHub

---
+++ 백준/Bronze/24086. 身長 （Height）/身長 （Height）.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int A = Integer.parseInt(br.readLine());
+        int B = Integer.parseInt(br.readLine());
+        
+        System.out.println(B - A);
+    }
+}
